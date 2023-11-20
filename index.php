99be4316430d747ed55cd1b1f4a10185275c8f89
Pierre Nogaro <pierre.nogaro@gmail.com>
2023-11-20 15:38:03 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Intro PHP</title>
+</head>
+<body>
+
+<h2>coucou</h2>
+
+<?php
+
+$age = 19;
+
+if ($age < 18){
+    echo "tu as $age ans donc tu es mineur";
+} else {
+    echo "tu as $age ans donc tu es majeur";
+}
+
+?>
+
+</body>
+</html>
+
+
+
